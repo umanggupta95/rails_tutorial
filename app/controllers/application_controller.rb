9584bd7ab6720e6fa5742ddf791f08f80b08337e
Umang <umanggupta.iet@gmail.com>
2017-10-02 17:25:22 +0530

    Changed readme file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: '¡Hola, mundo!'
+    render html: '¡Hola, mundo!'
   end
 end
